6b9a0aefec4faac04cfdfadf826e7ea8aab8319a
Mitish Raina <rainamitish7@gmail.com>
2025-03-13 22:51:27 +0530

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
-import { useState } from 'react'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import { logo } from './assets'
-import { Home, CreatePost } from './pages'
+import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router-dom';
+import { logo } from './assets';
+import { Home, CreatePost } from './pages';
+import { motion } from 'framer-motion';
 
-function App() {
-  
+
+function AppContent() {
+  const location = useLocation();
 
   return (
-    <BrowserRouter>
-      <header className='w-full flex justify-between items-center gradient-bg sm:px-8 px-4 py-4'>
-        <Link to='/ArtSquirt'>
-          <img src={logo} alt='logo' className='w-60 mt-2 object-contain' />
+    <>
+      <header className="w-full flex justify-between items-center gradient-bg sm:px-8 px-4 py-4">
+        <Link to="/ArtSquirt">
+          <motion.img
+            src={logo}
+            alt="logo"
+            className="w-60 mt-2 object-contain"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          />
         </Link>
-        <Link to='/ArtSquirt/create-post' className='font-inter font-medium bg-[#ffa371] text-white px-4 py-2 rounded-md blue-glassmorphism'>
-          Create Post
-        </Link>
+        <motion.div
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5 }}>
+          <Link
+            to="/ArtSquirt/create-post"
+            className="font-inter font-medium bg-[#ffa371] text-white px-4 py-2 rounded-md blue-glassmorphism"
+          >
+            Create Post
+          </Link>
+        </motion.div>
       </header>
-      <main className='sm:p-8 px-4 py-8 w-full bg-body min-h-[calc(100vh-73px)]'>
-        <Routes>
-          <Route path='/ArtSquirt' element={<Home />} />
-          <Route path='/ArtSquirt/create-post' element={<CreatePost />} />
-        </Routes>
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-body min-h-[calc(100vh-73px)]">
+        <motion.div
+          key={location.key}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Routes location={location}>
+            <Route path="/ArtSquirt" element={<Home />} />
+            <Route path="/ArtSquirt/create-post" element={<CreatePost />} />
+          </Routes>
+        </motion.div>
       </main>
-    </BrowserRouter>
-  )
+    </>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
+
+export default App;
