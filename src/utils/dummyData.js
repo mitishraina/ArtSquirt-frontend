9632c4af97c375de657014482173b2e216779226
Mitish Raina <rainamitish7@gmail.com>
2025-03-14 13:05:58 +0530

    .

---
+++ src/utils/dummyData.js
@@ -0,0 +1,87 @@
+import dancing_lion from "../assets/dancing_lion.png";
+import elephant_ride from "../assets/elephant_ride.png";
+import futuristic_towers from "../assets/futuristic_towers.png";
+import shiba_inu from "../assets/shiba_inu.png";
+import macro_35mm_mices from "../assets/macro_35mm_mices.png";
+import hedgehog from "../assets/hedgehog.jpg";
+import airplane from "../assets/airplane.jpg";
+import capybara from "../assets/capybara.jpg";
+import lord_krishna from "../assets/lord_krishna.jpg";
+import samurai from "../assets/samurai.jpg";
+import saviour from "../assets/saviour.jpg";
+import street from "../assets/street.jpg";
+
+export default [
+  {
+    _id: '1',
+    name: 'Lord Krishna',
+    prompt: 'Lord Krishna on a battlefield',
+    photo: lord_krishna,
+  },
+  {
+    _id: '2',
+    name: 'Elephant Ride',
+    prompt: 'An elephant giving rides in air',
+    photo: elephant_ride,
+  },
+  {
+    _id: '3',
+    name: 'Futuristic Towers',
+    prompt: 'Two futuristic towers with a skybridge covered in lush foliage, digital art',
+    photo: futuristic_towers,
+  },
+  {
+    _id: '4',
+    name: 'Shiba Inu',
+    prompt: 'a fortune-telling shiba inu reading your fate in a giant hamburger, digital art',
+    photo: shiba_inu,
+  },
+  {
+    _id: '5',
+    name: 'Macro Mice',
+    prompt: 'a macro 35mm photograph of two mice in Hawaii, theyre each wearing tiny swimsuits and are carrying tiny surf boards, digital art',
+    photo: macro_35mm_mices,
+  },
+  {
+    _id: '6',
+    name: 'Hedgehog',
+    prompt: 'A Synthwave Hedgehog, Blade Runner Cyberpunk',
+    photo: hedgehog,
+  },
+  {
+    _id: '7',
+    name: 'Dancing Lion',
+    prompt: 'A lion dancing with trump in India',
+    photo: dancing_lion,
+  },
+  {
+    _id: '8',
+    name: 'King Capybara',
+    prompt: 'an oil painting portrait of a capybara wearing medieval royal robes and an ornate crown on a dark background',
+    photo: capybara,
+  },
+  {
+    _id: '9',
+    name: 'Samurai',
+    prompt: 'A Samurai riding a Horse on Mars, lomography.',
+    photo: samurai,
+  },
+  {
+    _id: '10',
+    name: 'Airplane above stadium',
+    prompt: 'A 3D render of a rainbow colored hot air balloon flying above a reflective lake',
+    photo: airplane,
+  },
+  {
+    _id: '11',
+    name: 'Saviour',
+    prompt: 'The long-lost Star Wars 1990 Japanese Anime',
+    photo: saviour,
+  },
+  {
+    _id: '12',
+    name: 'Tokyo street',
+    prompt: 'A man walking through the bustling streets of Kowloon at night, lit by many bright neon shop signs, 50mm lensl',
+    photo: street,
+  },
+];
