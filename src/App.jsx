1a4439b660950bbcacde65b45b000d66cb8dcce9
Mitish Raina <rainamitish7@gmail.com>
2025-03-14 17:21:46 +0530

    Fixed issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Link, Route, Routes, useLocation } from 'react-router-dom';
 import { logo } from './assets';
-import { Home, CreatePost } from './pages';
+import { Home, CreatePost, Login } from './pages';
 import { motion } from 'framer-motion';
-
+import user from './images/user-round.svg';
 
 function AppContent() {
   const location = useLocation();
@@ -10,7 +10,7 @@ function AppContent() {
   return (
     <>
       <header className="w-full flex justify-between items-center gradient-bg sm:px-8 px-4 py-4">
-        <Link to="/ArtSquirt/">
+        <Link to="/ArtSquirt-frontend/">
           <motion.img
             src={logo}
             alt="logo"
@@ -20,17 +20,30 @@ function AppContent() {
             transition={{ duration: 0.5 }}
           />
         </Link>
-        <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5 }}>
-          <Link
-            to="/ArtSquirt/create-post"
-            className="font-inter font-medium bg-[#ffa371] text-white px-4 py-2 rounded-md blue-glassmorphism"
-          >
-            Create Post
-          </Link>
-        </motion.div>
+        <div className='flex gap-5'>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}>
+            <Link
+              to="/ArtSquirt-frontend/create-post"
+              className="font-inter font-medium bg-[#ffa371] text-white px-4 py-2 rounded-md blue-glassmorphism"
+            >
+              Create Post
+            </Link>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}>
+            <Link
+              to="/ArtSquirt-frontend/login"
+              className="h-2 w-2"
+            >
+              <img src={user} alt="user" className="w-8 h-8" />
+            </Link>
+          </motion.div>
+        </div>
       </header>
 
       <main className="sm:p-8 px-4 py-8 w-full bg-body min-h-[calc(100vh-73px)]">
@@ -39,11 +52,12 @@ function AppContent() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 1 }}
         >
           <Routes location={location}>
-            <Route path="/ArtSquirt/" element={<Home />} />
-            <Route path="/ArtSquirt/create-post" element={<CreatePost />} />
+            <Route path="/ArtSquirt-frontend" element={<Home />} />
+            <Route path="/ArtSquirt-frontend/create-post" element={<CreatePost />} />
+            <Route path="/ArtSquirt-frontend/login" element={<Login />} />
           </Routes>
         </motion.div>
       </main>
